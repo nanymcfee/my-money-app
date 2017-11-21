86c802df4ce1b78233773f8d1ca65f8d6f500630
nanymcfee <nanymcfee@atlassia.com>
2017-11-21 20:34:14 +0800

    add new investment

--- js/app.js
+++ js/app.js
@@ -16,10 +16,27 @@ var AppViewModel=function(){
     return this.curInvestment().roundNum()*this.curInvestment().firstHand()-this.curInvestment().currentVal()},
     this);
     //add new stock into the plan
-  this.addInvestment=function(investment){
-    this.invsetments.push({
-
+    this.itemToAdd=ko.observable({
+      firstHand:ko.observable(0),
+      roundNum:ko.observable(0),
+      Name:ko.observable(""),
+      currentVal:ko.observable(0),
     });
+    this.itemName=ko.observable("");//t store the name
+//add new inverment item
+  this.addInvestment=function(){
+    if(this.itemName().length>0){
+      var name=this.itemName();
+      this.itemToAdd().Name(name);
+      this.investments.push(this.itemToAdd());
+    }
+    console.log(this.investments());
+    this.itemName=ko.observable("");
+
+
+    //this.itemToAdd().Name("");
+    //console.log(this.investments());
+
 
   };
 

e761b80ff69eaffcd35c394adb3859a3377f8919
nanymcfee <nanymcfee@atlassia.com>
2017-12-01 09:03:29 +0800

    comlete add new stork function

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
 var AppViewModel=function(){
-  this.investments=ko.observableArray([{
-    firstHand:ko.observable(500),
-    roundNum:ko.observable(0),
-    Name:ko.observable("50AH"),
-    currentVal:ko.observable(0),
-  },{
-    firstHand:ko.observable(300),
-    roundNum:ko.observable(0),
-    Name:ko.observable("红利基金"),
-    currentVal:ko.observable(0),
-  }]);
+
+    this.investments=ko.observableArray([{
+      firstHand:ko.observable(500),
+      roundNum:ko.observable(0),
+      Name:ko.observable("50AH"),
+      currentVal:ko.observable(0),
+    },{
+      firstHand:ko.observable(300),
+      roundNum:ko.observable(0),
+      Name:ko.observable("红利基金"),
+      currentVal:ko.observable(0),
+    }]);
+
+  //this.investments=ko.observableArray(JSON.parse(localStorage.investments));
+
   this.curInvestment=ko.observable(this.investments()[0]);
 
   this.currentHand=ko.computed(function(){
@@ -29,18 +33,53 @@ var AppViewModel=function(){
       var name=this.itemName();
       this.itemToAdd().Name(name);
       this.investments.push(this.itemToAdd());
+      //update the invetments
+
     }
     console.log(this.investments());
     this.itemName=ko.observable("");
 
-
-    //this.itemToAdd().Name("");
-    //console.log(this.investments());
-
-
   };
 
 
-}
+};
 //Acitivats knock out
-ko.applyBindings(new AppViewModel());
+var myViewModel=new AppViewModel();
+
+ko.applyBindings(myViewModel);
+//store the itmes
+var storeInvestments=function(){
+  var ins=[];
+  myViewModel.investments.foreach(function(item){
+    var m= new {
+      firstHand:item.firstHand(),
+      roundNum:item.roundNum(),
+      Name:item.Name(),
+      currentVal:item.currentVal(),
+    };
+    ins.push(m);
+
+  });
+  localStorage.attendance=JSON.stringify(ins);
+};
+//read the items
+var readInvestments=function(memory){
+  for (var i=0;i<memory.length();i++){
+    myViewModel.investments[i].firstHand(memory[i].firstHand);
+    myViewModel.investments[i]
+  }
+
+}
+
+$(function(){
+  //console.log(localStorage.investments());
+
+  if(localStorage.investments){
+    myViewModel.investments(JSON.parse(localStorage.investments));
+  }else{
+    localStorage.investments=JSON.stringify(myViewModel.investments());
+  }
+  $("#addNew").on('click',function(){
+    localStorage.investments=JSON.stringify(myViewModel.investments());
+  });
+}());
